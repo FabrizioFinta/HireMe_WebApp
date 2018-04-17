514a998c18f9be93b6c68e44a63f4b1074d97583
attilamandoki <attilamandoki@gmail.com>
2018-04-17 13:49:10 +0200

    init(app) initial commit: created plain project

---
+++ src/main/java/com/greenfoxacademy/hire_meweb_app/HireMeWebAppApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.hire_meweb_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HireMeWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HireMeWebAppApplication.class, args);
+	}
+}
---
+++ src/test/java/com/greenfoxacademy/hire_meweb_app/HireMeWebAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.greenfoxacademy.hire_meweb_app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HireMeWebAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
